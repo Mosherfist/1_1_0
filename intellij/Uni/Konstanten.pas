5e57cc81647501ce4e6c638ff73591cc18761e3e
Uwe Staub <uwe.staub@yahoo.de>
2021-10-06 18:28:40 +0200

    Zweite s Programm für Konstanten

---
+++ intellij/Uni/Konstanten.pas
@@ -0,0 +1,9 @@
+program Konstanten (output);
+{ Programmbeispiel für eine Textkonstante }
+  const 
+    LEER5 = '     ';
+    LINIE5 = ' _____';
+    GRUSS = 'Guten Tag';
+begin
+  WriteLn(GRUSS, LINIE5,' Viel Spass', LEER5, LINIE5)
+end. {Konstanten}
