2929a229ee3f872600c30111a90ebd22bfe4fe70
Uwe Staub <uwe.staub@yahoo.de>
2021-10-09 13:35:38 +0200

    Beispiel 3.6.2.2 added

---
+++ Beispiel3.6.2.2.pas
@@ -0,0 +1,33 @@
+program Beispiel (output);
+
+const
+N = 15;
+L = '---------------------------';
+var
+i : integer;
+x,
+y :real;
+c :char;
+
+begin
+	i :=7;
+	x := -2.7;
+	y :=8.1;
+	c := '$';
+	write (1);
+	write (':');
+	write ('i*n=');
+	write (i:2);
+	write (c:3);
+	write (N:3);
+	write ('=':3);
+	write (i*N:3);
+	write ('2.':3);
+	write ('x*y=');
+	write (x:6:2);
+	write (c:3);
+	write (y:8:2);
+	write ('=':3);
+	writeln (x*y:7:1);
+	write (L, L);
+end.
